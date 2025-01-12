ba27e89743a46be16fc47f130a0b8893076d0700
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-12 14:22:59 +0200

    Create external.sh
    add external repos manually until troubleshooting of submodules

---
+++ external.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/miniupnp/miniupnp external/miniupnp
+git clone https://github.com/Tencent/rapidjson external/rapidjson
+git clone https://github.com/trezor/trezor-common.git external/trezor-common
+git clone https://github.com/monero-project/supercop external/supercop
+git clone -b 1.2.1-wow https://codeberg.org/wownero/RandomWOW external/RandomWOW
