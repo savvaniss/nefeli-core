5c62fa7c5202e8109b8093d01a9fe84c7453405e
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-12 18:55:35 +0200

    update genesis

--- external.sh
+++ external.sh
@@ -2,4 +2,4 @@ git clone https://github.com/miniupnp/miniupnp external/miniupnp
 git clone https://github.com/Tencent/rapidjson external/rapidjson
 git clone https://github.com/trezor/trezor-common.git external/trezor-common
 git clone -b monero https://github.com/monero-project/supercop external/supercop
-git clone -b 1.2.1-wow https://codeberg.org/wownero/RandomWOW external/RandomWOW
+git clone -b 1.2.1-wow https://codeberg.org/wownero/RandomWOW external/randomwow
